ccec46339780b6fde9d665260bc4be028025719e
yunusoruk <orukyunus@gmail.com>
2023-12-02 09:09:01 +0300

    initial commit

--- config/admin.ts
+++ config/admin.ts
@@ -1,18 +1,14 @@
 import { AdminConfig } from "@/types";
 
-export const adminConfig: AdminConfig ={
-    roles: [
-        {
-            id: '1',
-            name: "admin",
-        },
-        {
-            id: '2',
-            name: "interviewer",
-        },
-        {
-            id: '3',
-            name: "user",
-        },
-    ]
-}
+export const adminConfig: AdminConfig = {
+  roles: [
+    {
+      id: "1",
+      name: "admin",
+    },
+    {
+      id: "3",
+      name: "user",
+    },
+  ],
+};
